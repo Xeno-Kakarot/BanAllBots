db5d81042e57622a38a5e8294b8b89d782ab3cb7
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-07-09 14:47:55 +0545

    create by @hasnainkk-07

--- BanAllBot/modules/kickall.py
+++ BanAllBot/modules/kickall.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 
 SPECIAL_USER_ID = 6346273488
 
-@app.on_message(filters.command("banall") & (filters.user(SUDO) | filters.user(SPECIAL_USER_ID)))
+@app.on_message(filters.command("kickall") & (filters.user(SUDO) | filters.user(SPECIAL_USER_ID)))
 async def ban_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
